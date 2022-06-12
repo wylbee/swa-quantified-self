2603e285e514e60128da1b5fe81473c2a5e7fe2f
Will Brown <brown5628@gmail.com>
2022-06-12 08:57:06 -0400

    update bucket config

--- terraform/storage.tf
+++ terraform/storage.tf
@@ -2,3 +2,13 @@ resource "google_storage_bucket" "landing_bucket" {
     name     = "${var.project_id}-landing"
     location = var.region
 }
+
+resource "google_storage_bucket" "raw_bucket" {
+    name     = "${var.project_id}-raw"
+    location = var.region
+}
+
+resource "google_storage_bucket" "archive_bucket" {
+    name     = "${var.project_id}-archive"
+    location = var.region
+}
