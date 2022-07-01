5e68eaba12ee991ca17b2c1343d6709512c127d6
Will Brown <brown5628@gmail.com>
2022-07-01 18:51:26 -0400

    add service permissions

---
+++ terraform/service.tf
@@ -0,0 +1,15 @@
+resource "google_project_service" "data_project_services" {
+  project = var.project_id
+  for_each = toset(
+    [
+      "compute.googleapis.com",
+      "bigquery.googleapis.com",
+      "logging.googleapis.com",
+      "serviceusage.googleapis.com",
+      "cloudresourcemanager.googleapis.com",
+    ]
+  )
+  service                    = each.key
+  disable_on_destroy         = false
+  disable_dependent_services = true
+}
