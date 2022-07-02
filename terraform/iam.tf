482c25ea8b3fec44cf25ea5e12c217d85a2e8e99
Will Brown <brown5628@gmail.com>
2022-07-02 15:53:18 -0400

    bucket permissions

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -6,3 +6,12 @@ resource "google_project_iam_binding" "sa_iam_bindings" {
     "serviceAccount:${google_service_account.dbt_sa.email}",
   ]
 }
+
+resource "google_storage_bucket_iam_member" "raw_bucket" {
+  bucket = "${var.project_id}-raw"
+  role   = "roles/storage.objectViewer"
+  members = [
+    "serviceAccount:${google_service_account.dbt_sa.email}",
+  ]
+  depends_on = [google_storage_bucket.raw_bucket]
+}
