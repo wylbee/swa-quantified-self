7b207faba6f885fa52e6394755aff3af81fb5dd4
Will Brown <brown5628@gmail.com>
2022-07-06 19:13:15 -0400

    add cf sa

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -13,3 +13,10 @@ resource "google_storage_bucket_iam_member" "raw_bucket" {
   member = "serviceAccount:${google_service_account.dbt_sa.email}"
   depends_on = [google_storage_bucket.raw_bucket]
 }
+
+resource "google_storage_bucket_iam_member" "raw_bucket" {
+  bucket = "${var.project_id}-raw"
+  role   = "roles/storage.objectAdmin"
+  member = "serviceAccount:${google_service_account.cf_sa.email}"
+  depends_on = [google_storage_bucket.raw_bucket]
+}
--- terraform/service_account.tf
+++ terraform/service_account.tf
@@ -16,3 +16,16 @@ resource "google_service_account_key" "dbt_sa_key" {
   service_account_id = google_service_account.dbt_sa.name
   public_key_type    = "TYPE_X509_PEM_FILE"
 }
+
+# cf service account
+resource "google_service_account" "cf_sa" {
+  account_id   = "cf-runner"
+  project      = var.project_id
+  display_name = "Cloud Function Service Account"
+  description  = "Cloud Function service account"
+}
+# cf service account key
+resource "google_service_account_key" "cf_sa_key" {
+  service_account_id = google_service_account.cf_sa.name
+  public_key_type    = "TYPE_X509_PEM_FILE"
+}
