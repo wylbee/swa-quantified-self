5958df684f971718274d6cac9a5311f3019cfad8
Will Brown <brown5628@gmail.com>
2022-07-06 19:15:45 -0400

    rename resource

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -7,14 +7,14 @@ resource "google_project_iam_binding" "sa_iam_bindings" {
   ]
 }
 
-resource "google_storage_bucket_iam_member" "raw_bucket" {
+resource "google_storage_bucket_iam_member" "raw_bucket_view" {
   bucket = "${var.project_id}-raw"
   role   = "roles/storage.objectViewer"
   member = "serviceAccount:${google_service_account.dbt_sa.email}"
   depends_on = [google_storage_bucket.raw_bucket]
 }
 
-resource "google_storage_bucket_iam_member" "raw_bucket" {
+resource "google_storage_bucket_iam_member" "raw_bucket_admin" {
   bucket = "${var.project_id}-raw"
   role   = "roles/storage.objectAdmin"
   member = "serviceAccount:${google_service_account.cf_sa.email}"
