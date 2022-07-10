587d8d64b323089a6b7ea2b5db4b99e4c93c87d1
Will Brown <brown5628@gmail.com>
2022-07-10 15:01:42 -0400

    attempt debug

--- cloud_functions/extract_gcs_load_gcs/main.py
+++ cloud_functions/extract_gcs_load_gcs/main.py
@@ -1,16 +1,25 @@
 # %%
 import os
+from google.cloud import storage
 from infer_date_from_filename import infer_date_from_filename
 from unzip_file import unzip_file
 from load_loop_habit import load_loop_habit
 
 def extract_gcs_load_gcs(event, context):
 #def extract_gcs_load_gcs(source_path:str, unizp_destination_path:str, processed_destination_path: str):
-    source_path = os.path.join('gs://', event['bucket'], event['name'])
+    source_uri = os.path.join('gs://', event['bucket'], event['name'])
+    print(source_uri)
+    source_path = f"tmp/zip/{event['name']}"
+    print(source_path)
     unizp_destination_path = 'tmp/dump'
     processed_destination_path = 'tmp/clean'
+
+    client = storage.Client()
+    bucket = client.get_bucket('qs-dev-352513-raw')
+    blob = bucket.blob(source_path)
+    blob.download_to_filename(source_path)
+
+    
     inferred_date = infer_date_from_filename(source_path)
     unzip_file(source_path, unizp_destination_path)
     load_loop_habit(unizp_destination_path, processed_destination_path, inferred_date)
-
-
--- terraform/cloud_function.tf
+++ terraform/cloud_function.tf
@@ -8,7 +8,7 @@ data "archive_file" "source" {
 }
 
 resource "random_string" "r" {
-  length  = 19
+  length  = 18
   special = false
 }
 
