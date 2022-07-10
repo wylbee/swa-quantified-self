3b470c332e2840511c540d841843c5eee113b3af
Will Brown <brown5628@gmail.com>
2022-07-10 08:45:12 -0400

    resolve error

--- terraform/cloud_function.tf
+++ terraform/cloud_function.tf
@@ -1,7 +1,15 @@
+resource "google_storage_bucket_object" "archive" {
+  name   = "index.zip"
+  bucket = google_storage_bucket.archive_bucket.name
+  source = "../cloud_functions/extract_gcs_load_gcs"
+}
+
 resource "google_cloudfunctions_function" "extract_gcs_load_gcs" {
     name = "function-extract-gcs-load-gcs"
     runtime = "python39"
     entry_point = "extract_gcs_load_gcs"
+    source_archive_bucket = google_storage_bucket.archive_bucket.name
+    source_archive_object = google_storage_bucket_object.archive.name
     event_trigger {
       event_type = "google.storage.object.finalize"
       resource = "${var.project_id}-landing"
