e3e0745dea6a3d5360d7c5b79d1b351941c0336a
Will Brown <brown5628@gmail.com>
2022-07-15 21:07:50 -0400

    ?

--- dbt/swa_quantified_self/models/staging/loop_habits/corrected_zone/corz_loop_habits__habits.sql
+++ dbt/swa_quantified_self/models/staging/loop_habits/corrected_zone/corz_loop_habits__habits.sql
@@ -14,19 +14,64 @@ parsed as (
 
 ),
 
-final as (
+cleaned as (
 
     select
-        * except (id_habit),
         case
             when id_habit = 'L0H24' then 'LH024'
             when id_habit = 'LH24' then 'LH024'
             when id_habit = 'LH23' then 'LH023'
             else id_habit
-        end as id_habit
+        end as id_habit,
+
+         {{ dbt_utils.surrogate_key([
+            'Position', 
+            'Name',
+            'Question',
+            'Description',
+            'NumRepetitions',
+            '`Interval`',
+            'Color',
+            'id_habit',
+        ]) }} as id_meta_row_check_hash,       
+
+        Position as val_habit_position,
+        Name as str_habit_name,
+        Question as str_habit_prompt,
+        Description as str_habit_description,
+        NumRepetitions as val_habit_target,
+        `Interval` as val_habit_interval,
+        Color as str_habit_color_hex,
+        dt_processed as dt_meta_exported,
+        str_gcs_file_name as str_meta_file_name
     
     from parsed
 
+),
+
+change_event as (
+
+    select
+        *,
+        case 
+            when coalesce(lag(id_meta_row_check_hash) over (
+            partition by id_habit
+            order by dt_meta_exported
+        ), 'new_row') != id_meta_row_check_hash then dt_meta_exported
+        end as dt_meta_change_event
+        
+    from cleaned
+
+),
+
+final as (
+
+    select
+        * except (dt_meta_change_event),
+        min(dt_meta_change_event) over (partition by id_meta_row_check_hash) as dt_meta_last_change_event
+    from change_event
+
 )
 
-select * from final order by id_habit
+
+select * from final order by id_habit, dt_meta_exported
--- dbt/swa_quantified_self/models/staging/loop_habits/scd2/scd2_loop_habits__habits.sql
+++ dbt/swa_quantified_self/models/staging/loop_habits/scd2/scd2_loop_habits__habits.sql
@@ -1,51 +1,60 @@
 {{ config(
-    materialized = 'table'
+    materialized = 'incremental',
+    unique_key = 'id_meta_row_check_hash',
+    merge_update_columns = ['dt_valid_to']
 ) }}
 
-with corrected as (
+
+with all_or_new as (
 
     select * from {{ ref('corz_loop_habits__habits') }}
 
+    {% if is_incremental() %}
+    
+        -- get the latest day of data from when this model was last run.
+        -- always reprocess that day and the day before (just to be safe)
+        -- plus anything newer, of course
+    
+        where dt_meta_exported >= datetime_sub(
+            cast( (select max(dt_meta_last_change_event) from {{ this }}) as datetime),
+        interval 1 day
+        )
+    
+    {% endif %}
 
 ),
 
-row_hash as (
+dates as (
 
     select
-        *,
-        {{ dbt_utils.surrogate_key([
-            'position', 
-            'Name',
-            'Question',
-            'Description',
-            'NumRepetitions',
-            '`Interval`',
-            'Color',
-            'id_habit',
-        ]) }} as id_row_check_hash
+        id_habit,
+        id_meta_row_check_hash,
+        dt_meta_last_change_event,
+        lead(dt_meta_last_change_event) over (partition by id_habit order by dt_meta_last_change_event) as dt_meta_next_change_event
 
-    from corrected
+    from all_or_new
 
-    qualify coalesce(lag(id_row_check_hash) over (
-            partition by id_habit
-            order by dt_processed
-        ), 'new_row') != id_row_check_hash
+    group by 1,2,3
 
 ),
 
-final as (
+windowed as (
 
-    select *,
-            {{ dbt_utils.surrogate_key(['id_habit', 'dt_processed']) }} as id_scd,
-        case
-            when dt_processed = '2022-06-12' then '2022-01-01'
-            else dt_processed
-        end as dt_valid_from,
-        lead(dt_processed) over (partition by id_habit order by dt_processed) as dt_valid_to
+    select 
+        all_or_new.*,
+        dates.dt_meta_last_change_event as dt_valid_from,
         
-    from row_hash
+        -- valid_to should be null if this id_habit_scd appears in the most recent day of data
+        -- (i.e. the record is still valid)
+        dt_meta_next_change_event as dt_valid_to
+        
+    from all_or_new
+    --{{ dbt_utils.group_by(n=13) }}
+    left outer join dates 
+        on all_or_new.id_meta_row_check_hash = dates.id_meta_row_check_hash
 
 )
 
-
-select * from final
+select * from windowed
+-- deduplicate: only take the latest day of data for each id_habit_scd
+qualify row_number() over (partition by id_meta_row_check_hash order by dt_meta_exported) = 1
