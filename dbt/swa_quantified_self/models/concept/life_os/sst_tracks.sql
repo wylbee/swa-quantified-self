225ce35a0d80254b55b65ed0972ef8dbf5c5abfc
Will Brown <brown5628@gmail.com>
2022-07-17 19:34:32 -0400

    add tracks boolean

--- dbt/swa_quantified_self/models/concept/life_os/sst_tracks.sql
+++ dbt/swa_quantified_self/models/concept/life_os/sst_tracks.sql
@@ -35,6 +35,14 @@ with
             tracks.dt_track,
             tracks.val_track,
             scores.val_track_score,
+            case
+                when tracks.val_track = 2
+                then 1
+                when tracks.id_habit = 'LH024' and tracks.val_track = 5000
+                then 1
+                else 0
+            end as is_track_completed,
+            case when tracks.val_track = 1 then 1 else 0 end as is_track_planned_rest,
             tracks.id_meta_row_check_hash as id_meta_tracks_row_check_hash,
             scores.id_meta_row_check_hash as id_meta_scores_row_check_hash
 
@@ -42,7 +50,16 @@ with
 
         left outer join scores on tracks.id_track = scores.id_track
 
-    )
+    ),
 
+    final as (
+
+        select
+            *,
+            case
+                when is_track_completed = 1 or is_track_planned_rest = 1 then 1 else 0
+            end as is_track
+        from joined
+    )
 select *
-from joined
+from final
