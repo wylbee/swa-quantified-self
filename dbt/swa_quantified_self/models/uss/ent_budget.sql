9de3e4caa70a953d26152b38c65bb9b52e5d7974
Will Brown <brown5628@gmail.com>
2023-03-07 06:40:44 -0500

    add bools

--- dbt/swa_quantified_self/models/uss/ent_budget.sql
+++ dbt/swa_quantified_self/models/uss/ent_budget.sql
@@ -2,7 +2,33 @@ with
 
     base as (select * from {{ ref("base_tiller__categories") }}),
 
-    final as (select id_tiller_budget as key_budget, * from base)
+    final as (
+        select
+            id_tiller_budget as key_budget,
+            id_tiller_budget,
+            cat_budget_group,
+            cat_budget_type,
+            is_budget_hidden_from_reporting,
+            is_budget_monthly_line,
+
+            case
+                when cat_budget_type like '%Income%' then 1 else 0
+            end as is_budget_income,
+
+            case
+                when cat_budget_group like '%Save%' then 1 else 0
+            end as is_budget_save,
+
+            case
+                when cat_budget_group like '%Live%'
+                then 1
+                when cat_budget_group like '%Give%'
+                then 1
+                else 0
+            end as is_budget_income
+
+        from base
+    )
 
 select *
 from final
