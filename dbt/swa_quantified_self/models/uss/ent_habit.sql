5ed10253234aa520145d2eefc4d48d2b8ca3c63d
Will Brown <brown5628@gmail.com>
2023-03-21 07:22:42 -0400

    initial docs

--- dbt/swa_quantified_self/models/uss/ent_habit.sql
+++ dbt/swa_quantified_self/models/uss/ent_habit.sql
@@ -2,7 +2,15 @@ with
 
     habits as (select * from {{ ref("base_loop_habits__habits") }}),
 
-    final as (select id_habit as key_habit, * from habits)
+    final as (
+        select
+            id_habit as key_habit,
+            *,
+            case
+                when str_habit_description = '#PENTATHALON' then 1 else 0
+            end as is_habit_pentathalon
+        from habits
+    )
 
 select *
 from final
